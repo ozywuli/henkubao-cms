07ebc6658067badeb97742df52cd20ec4507e357
Ozy Wu-Li <ozy.wu.li@gmail.com>
2018-05-08 15:37:19 -0700

    add basic login functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :getSettings
 
+  protect_from_forgery with: :exception
+  include SessionsHelper
+
   protected
 
-  def getSettings
-    @setting = Setting.first
-  end
+    def getSettings
+      @setting = Setting.first
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,14 @@ Rails.application.routes.draw do
   get '/settings', to: 'settings#index'
 
   get '/signup', to: 'users#new'
-   post '/signup',  to: 'users#create'
+  post '/signup',  to: 'users#create'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
   get '/admin', to: 'admin#index'
-  # get '/admin/settings', to: 'settings#index'
+  get '/admin/settings', to: 'settings#index'
 
   resources :settings
   resources :posts, param: :slug
