94267a52f9bb7c67e33ea9e7123d885d6e8744b2
Ozy Wu-Li <ozy.wu.li@gmail.com>
2018-05-07 19:18:04 -0700

    add test signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,21 @@
 Rails.application.routes.draw do
-  resources :settings
-  resources :posts, param: :slug
-
   get 'home/index'
   get 'settings/index'
-  
-  root 'home#index'
 
+  get '/signup', to: 'users#new'
+   post '/signup',  to: 'users#create'
+
+  get '/admin', to: 'admin#index'
+  get '/admin/settings', to: 'settings#index'
+
+  resources :settings
+  resources :posts, param: :slug
+  resources :users
+
+  root 'home#index'
+  
   # Ember
   # get "/guides/:name'", to: redirect('/guides/%{name}')
   # Catch all for HTML 5 history routing. This must be the last route.
   # get '/*path', to: 'guides#index', format: false
-
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_05_192132) do
+ActiveRecord::Schema.define(version: 2018_05_07_215411) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,15 @@ ActiveRecord::Schema.define(version: 2018_05_05_192132) do
     t.datetime "updated_at", null: false
     t.bigint "setting_id"
     t.index ["setting_id"], name: "index_social_addresses_on_setting_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "social_addresses", "settings"
